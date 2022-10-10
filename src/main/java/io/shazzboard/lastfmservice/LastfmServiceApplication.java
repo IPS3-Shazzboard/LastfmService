0b57bf08b854a693ae8613065073784149a747a5
Calvin Zhen <418224@student.fontys.nl>
2022-10-10 15:04:05 +0200

    Initial commit

---
+++ src/main/java/io/shazzboard/lastfmservice/LastfmServiceApplication.java
@@ -0,0 +1,13 @@
+package io.shazzboard.lastfmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastfmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastfmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/shazzboard/LastfmService/LastfmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shazzboard.lastfmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastfmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
